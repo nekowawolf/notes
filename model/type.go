b76aeca7265d58846a07278d07c599dd76d873f8
nekowawolf <irukainu20@gmail.com>
2024-11-23 23:36:18 +0700

    feat: add note struct

---
+++ model/type.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Note struct {
+	ID      primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`        
+	Title   string             `bson:"title,omitempty" json:"title,omitempty"`    
+	Content string             `bson:"content,omitempty" json:"content,omitempty"` 
+	Date    time.Time   	   `bson:"date,omitempty" json:"date,omitempty"` 
+}
