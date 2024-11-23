22b89e5ce61c943a7152477dab2bf24aac93c944
nekowawolf <irukainu20@gmail.com>
2024-11-23 23:37:08 +0700

    feat: add config

---
+++ module/config.go
@@ -0,0 +1,38 @@
+package module
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Database *mongo.Database
+
+func init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	mongoURI := os.Getenv("MONGOSTRING")
+	if mongoURI == "" {
+		fmt.Println("MONGOSTRING environment variable is not set")
+		os.Exit(1)
+	}
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		fmt.Printf("Failed to connect to MongoDB: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err = client.Ping(ctx, nil); err != nil {
+		fmt.Printf("Unable to ping MongoDB: %v\n", err)
+		os.Exit(1)
+	}
+
+	Database = client.Database("notes")
+	fmt.Println("Successfully connected to MongoDB")
+}
