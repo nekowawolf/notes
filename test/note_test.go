421a1e2363fb20f770ba08346371e72c5d9a43cc
nekowawolf <irukainu20@gmail.com>
2024-11-25 05:38:05 +0700

    test: add testing func note

--- test/note_test.go
+++ test/note_test.go
@@ -4,17 +4,77 @@ import (
 	"fmt"
 	"testing"
     "github.com/nekowawolf/notes/module"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func TestInsertNote(t *testing.T) {
-	title := "Unit Test Note"
-	content := "This is a test note for unit testing."
+func TestInsertNotes(t *testing.T) {
+	title := "Test"
+	content := "unit testing."
 
-	result, err := module.InsertNote(title, content)
+	result, err := module.InsertNotes(title, content)
 	if err != nil {
-		t.Errorf("Failed to insert note: %v", err)
+		t.Errorf("Failed to insert notes: %v", err)
 		return
 	}
 
-	fmt.Printf("Inserted Note ID: %v\n", result)
+	fmt.Printf("Inserted Notes ID: %v\n", result)
+}
+
+func TestGetAllNotes(t *testing.T) {
+	data, err := module.GetAllNotes()
+	if err != nil {
+		t.Errorf("Failed to retrieve notes: %v", err)
+	} else if len(data) == 0 {
+		t.Errorf("No notes found")
+	} else {
+		fmt.Printf("Retrieved notes: %v\n", data)
+	}
+}
+
+func TestGetNotesByID(t *testing.T) {
+	id, err := primitive.ObjectIDFromHex("674253f0e2a1fdaf1c25c333") 
+	if err != nil {
+		t.Fatalf("Invalid ObjectID: %v", err)
+	}
+
+	notes, err := module.GetNotesByID(id)
+	if err != nil {
+		t.Errorf("Failed to get notes: %v", err)
+	} else {
+		t.Logf("Retrieved Notes: %+v", notes)
+	}
+}
+
+func TestUpdateNotesByID(t *testing.T) {
+	id, err := primitive.ObjectIDFromHex("674253f0e2a1fdaf1c25c333")
+	if err != nil {
+		t.Fatalf("Invalid ObjectID: %v", err)
+	}
+
+	newTitle := "Test1"
+	newContent := "Testing"
+
+	updatedNote, err := module.UpdateNotesByID(id, newTitle, newContent)
+	if err != nil {
+		t.Errorf("Failed to update note: %v", err)
+		return
+	}
+
+	t.Logf("Successfully updated note: %+v", updatedNote)
+}
+
+func TestDeleteNotesByID(t *testing.T) {
+	id, err := primitive.ObjectIDFromHex("67439328c0edc2854b3a1828")
+	if err != nil {
+		t.Errorf("Invalid ID format: %v", err)
+		return
+	}
+
+	err = module.DeleteNotesByID(id)
+	if err != nil {
+		t.Errorf("Failed to delete note by ID: %v", err)
+		return
+	}
+
+	t.Logf("Note with ID %s deleted successfully", id.Hex())
 }
