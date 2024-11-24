b8fe0be2b4faf32b8197b1fc8c4f607a07b27011
nekowawolf <irukainu20@gmail.com>
2024-11-25 05:39:03 +0700

    feat: add func notes

--- module/function.go
+++ module/function.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nekowawolf/notes/config"
 	"github.com/nekowawolf/notes/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func InsertOneDoc(collection string, doc interface{}) interface{} {
@@ -19,18 +20,82 @@ func InsertOneDoc(collection string, doc interface{}) interface{} {
 }
 
 
-func InsertNote(title, content string) (interface{}, error) {
-	note := model.Note{
+func InsertNotes(title, content string) (interface{}, error) {
+	notes := model.Notes{
 		ID:      primitive.NewObjectID(),
 		Title:   title,
 		Content: content,
 		Date:    time.Now(),
 	}
 
-	insertResult, err := config.Database.Collection("notes").InsertOne(context.TODO(), note)
+	insertResult, err := config.Database.Collection("notes").InsertOne(context.TODO(), notes)
 	if err != nil {
 		return nil, err
 	}
 
 	return insertResult.InsertedID, nil
 }
+
+func GetAllNotes() ([]model.Notes, error) {
+	collection := config.Database.Collection("notes")
+
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		return nil, fmt.Errorf("GetAllNotes Find: %v", err)
+	}
+
+	var notes []model.Notes
+	if err = cursor.All(context.TODO(), &notes); err != nil {
+		return nil, fmt.Errorf("GetAllNotes All: %v", err)
+	}
+
+	return notes, nil
+}
+
+func GetNotesByID(id primitive.ObjectID) (model.Notes, error) {
+	collection := config.Database.Collection("notes")
+	var notes model.Notes
+	err := collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&notes)
+	if err != nil {
+		return model.Notes{}, err
+	}
+	return notes, nil
+}
+
+func UpdateNotesByID(id primitive.ObjectID, title string, content string) (model.Notes, error) {
+	collection := config.Database.Collection("notes")
+
+	update := bson.M{
+		"$set": bson.M{
+			"title":   title,
+			"content": content,
+		},
+	}
+
+	filter := bson.M{"_id": id}
+
+	var updatedNote model.Notes
+	err := collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&updatedNote)
+	if err != nil {
+		return model.Notes{}, err
+	}
+
+	return updatedNote, nil
+}
+
+func DeleteNotesByID(id primitive.ObjectID) error {
+    collection := config.Database.Collection("notes")
+    filter := bson.M{"_id": id}
+
+    result, err := collection.DeleteOne(context.TODO(), filter)
+    if err != nil {
+        return fmt.Errorf("error deleting note for ID %s: %s", id.Hex(), err.Error())
+    }
+
+    if result.DeletedCount == 0 {
+        return fmt.Errorf("no note found with ID %s", id.Hex())
+    }
+
+    return nil
+}
+
