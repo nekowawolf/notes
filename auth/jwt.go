db8645a449e5aca2c52a2daf4ba17f825f7d9ccb
nekowawolf <irukainu20@gmail.com>
2024-12-07 13:00:06 +0700

    feat: implement JWT authentication and password hashing functions

---
+++ auth/jwt.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"fmt" 
+	"time"
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(username string) (string, error) {
+	claims := &Claims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(48 * time.Hour)), 
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+func ValidateJWT(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, jwt.ErrInvalidKey
+	}
+
+	return claims, nil
+}
+
+func HashPassword(password string) (string, error) {
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    if err != nil {
+        return "", fmt.Errorf("failed to hash password: %w", err)
+    }
+    return string(hash), nil
+}
+
+func VerifyPassword(hashedPassword, plainPassword string) error {
+    return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+}
